7bf00549400d1ee1bd009a2cb04f58eb43a3caf3
SYBaydoun <shahin.baydoun@gmx.net>
2026-05-19 21:24:43 +0000

    scrollable buttons aber scroll triggerd noch klick

--- index.py
+++ index.py
@@ -24,6 +24,8 @@ menuLibrary = {"menu_items": ["Start New Game", "Continue Game", "Settings", "Cr
                "credits_items": ["Back"]
                }
 
+offset = 0
+
 
 MAP_SIZE = 10
 icon_size = (24, 24)
@@ -55,9 +57,7 @@ tiles = {
     3: "surface_dark",
 }
 
-class SceneManager():
-    dauer_bewegung = 120
-    dauer_ruhen = 10
+class SceneManager:
     def __init__(self, start_scene):
         self.scene = start_scene
         self.scene.on_enter()
@@ -68,6 +68,9 @@ class SceneManager():
         self.next_scene = None
         self.t = 0
         self.speed = 1
+
+        self.dauer_bewegung = 120
+        self.dauer_ruhen = 10
 
     def change_scene(self, new_scene):
         if self.transitioning:
@@ -85,41 +88,28 @@ class SceneManager():
 
         self.t += self.speed
 
-        # --------------------
-        # PHASE 1: CLOSE
-        # --------------------
+        # CLOSE
         if self.phase == 1:
-            if self.t == 1:
-                door_sound.stop()
-                door_sound.play()
-            if self.t >= self.dauer_bewegung: # >= damit bei einem overshoot durch eine speed die nicht perfekt passt trozdem stoppt
+            if self.t >= self.dauer_bewegung:
                 if hasattr(self.scene, "on_exit"):
                     self.scene.on_exit()
 
                 self.scene = self.next_scene
                 self.scene.on_enter()
 
-                self.phase = 2 # <- neue Phase
-                self.t = 0 # <- immerwieder zurücksetzen für nächsten Phaseniteration
+                self.phase = 2
+                self.t = 0
 
-        # --------------------
-        # PHASE 2: HOLD (geschlossen)
-        # --------------------
+        # HOLD
         elif self.phase == 2:
-            if self.t == self.dauer_ruhen //2:
-                door_sound.stop()
-            if self.t >= self.dauer_ruhen:   # <- dauer die gewartet wird bevor die türen wieder aufgehen
+            if self.t >= self.dauer_ruhen:
                 self.phase = 3
                 self.t = 0
 
-        # --------------------
-        # PHASE 3: OPEN
-        # --------------------
+        # OPEN
         elif self.phase == 3:
-            if self.t == 1:
-                door_sound.play()
             if self.t >= self.dauer_bewegung:
-                self.transitioning = False # transition ist vorbei
+                self.transitioning = False
                 self.phase = 0
                 self.t = 0
 
@@ -129,29 +119,26 @@ class SceneManager():
         if self.transitioning:
             self.draw_doors()
 
-    def on_mouse_down(self, pos):
+    def on_mouse_down(self, pos, button):
         if self.transitioning:
             return
 
+        if hasattr(self.scene, "scrollable_ui") and self.scene.scrollable_ui:
+            self.scene.scrollable_ui.handle_scroll(button)
+
         if hasattr(self.scene, "on_mouse_down"):
-            self.scene.on_mouse_down(pos)
+            self.scene.on_mouse_down(pos, button)
 
     def draw_doors(self):
         duration = 60
 
-        # CLOSE animation
         if self.phase == 1:
             progress = min(self.t / duration, 1)
-
-        # HOLD (fully closed)
         elif self.phase == 2:
             progress = 1
-
-        # OPEN animation
-        else:  # phase 3
+        else:
             progress = 1 - min(self.t / duration, 1)
 
-        # smoothstep
         progress = progress * progress * (3 - 2 * progress)
 
         top_h = door_top.get_height()
@@ -162,6 +149,7 @@ class SceneManager():
 
         screen.blit(door_top, (0, top_y))
         screen.blit(door_bottom, (0, bottom_y))
+
 
 class Button():
     def __init__(self, text, center):
@@ -245,9 +233,54 @@ class Button():
     def clicked(self, pos):
         return self.rect.collidepoint(pos)
     
-class ScrollableButtons():
+    def set_position(self, pos):
+        self.rect.center = pos
+    
+class ScrollableButtons:
     def __init__(self):
-        pass
+        self.buttons = []
+        self.upper_bound = 0
+        self.lower_bound = 0
+        self.offset = 0
+        self.scroll_speed = 20
+
+    def add_button(self, text):
+        self.buttons.append(Button(text, (WIDTH // 2, 0)))
+
+    def handle_scroll(self, button):
+        if button == mouse.WHEEL_UP:
+            self.offset = max(self.offset - self.scroll_speed, 0)
+
+        elif button == mouse.WHEEL_DOWN:
+            self.offset += self.scroll_speed
+
+    def update(self):
+        for b in self.buttons:
+            b.update()
+
+    def draw(self):
+        for i, button in enumerate(self.buttons):
+            y = self.upper_bound + i * 70 - self.offset
+
+            # culling (nur sichtbare Buttons zeichnen)
+            if self.upper_bound <= y <= self.lower_bound:
+                button.set_position((WIDTH // 2, y))
+                button.draw()
+
+    def on_click(self, pos):
+        for i, button in enumerate(self.buttons):
+            y = self.upper_bound + i * 70 - self.offset
+
+            if self.upper_bound <= y <= self.lower_bound:
+                button.set_position((WIDTH // 2, y))
+                if button.clicked(pos):
+                    return button.text
+
+        return None
+    
+    def update(self):
+        for button in self.buttons:
+            button.update()
         
 class Icon:
     def __init__(self, source, position, size=icon_size, bg_color=(20, 20, 35), border_color=(80, 80, 120)):
@@ -281,7 +314,7 @@ class Icon:
 #-------------------------------------------
 #Parent Classe Für die Menü Szenen
 class MenuSzene():
-    def __init__(self, items: str, buttons:str, draw_bg:str, title:str, text: str = "", input: bool = False):
+    def __init__(self, items:str, buttons:str, draw_bg:str, title:str, text: str = "", input: bool = False,scrollable: bool = False, scrollableItems: list[str] = []):
         self.items = menuLibrary[items]
         self.buttons = []
         self.draw_bg = draw_bg
@@ -289,6 +322,9 @@ class MenuSzene():
         self.text = text
         self.input = input
         self.input_text = ""
+        self.scrollable = scrollable
+        self.scrollableItems = scrollableItems
+        self.scrollable_ui = None
         self.setup()
     
     def setup(self):
@@ -337,6 +373,20 @@ class MenuSzene():
                     ocolor="black"
                 )
         
+        if self.scrollable and self.scrollable_ui is None:
+            self.scrollable_ui = ScrollableButtons()
+            for item in self.scrollableItems:
+                self.scrollable_ui.add_button(item)
+
+                if self.input:
+                    self.scrollable_ui.upper_bound = start_y + 100
+                else:
+                    self.scrollable_ui.upper_bound = start_y + 20
+                self.scrollable_ui.lower_bound = min(start_y + 400, HEIGHT - 100)
+
+                self.scrollable_ui.update()
+                self.scrollable_ui.draw()
+
         if self.input:
             input_rect = pygame.Rect(WIDTH // 2 - 200, start_y, 400, 50)
             pygame.draw.rect(screen.surface, (255, 255, 255), input_rect, border_radius=10)
@@ -363,7 +413,7 @@ class Menu(MenuSzene):
     def __init__(self):
         super().__init__("menu_items", "menu_buttons", "bg_menu.jpg", TITLE)
     
-    def on_mouse_down(self, pos):
+    def on_mouse_down(self, pos, button):
         for button in self.buttons:
             if button.clicked(pos):
                 if button.text == "Start New Game":
@@ -384,7 +434,7 @@ class NewGame(MenuSzene):
         super().__init__("new_items", "new_buttons", "bg_new.jpg", "Start New Game", input=True)
         self.current_save_path = None
     
-    def on_mouse_down(self, pos):
+    def on_mouse_down(self, pos, button):
         for button in self.buttons:
             if button.clicked(pos):
                 if button.text == "Back":
@@ -393,9 +443,9 @@ class NewGame(MenuSzene):
 #Szene Spiel fortsetzen
 class ContinueGame(MenuSzene):
     def __init__(self):
-        super().__init__("continue_items", "continue_buttons", "bg_continue.jpg", "Continue Game", input=True)
+        super().__init__("continue_items", "continue_buttons", "bg_continue.jpg", "Continue Game", input=True, scrollable=True, scrollableItems=[filename for filename in os.listdir("saved_games") if filename.endswith(".json")])
 
-    def on_mouse_down(self, pos):
+    def on_mouse_down(self, pos, button):
         for button in self.buttons:
              if button.clicked(pos):
                 if button.text == "Back":
@@ -403,12 +453,28 @@ class ContinueGame(MenuSzene):
                 elif button.text.startswith("Save"):
                     print(f"Continuing game from {button.text}...")
 
+        if self.scrollable_ui:
+            result = self.scrollable_ui.on_click(pos)
+            if result:
+                manager.change_scene(GameHomeBase(save_path=os.path.join("saved_games", result)))
+
+    def update(self):
+        super().update()
+        if self.scrollable_ui:
+            self.scrollable_ui.update()
+
+    def draw(self):
+        super().draw()
+        if self.scrollable_ui:
+            self.scrollable_ui.draw()
+
+
 #Szene Einstellungen
 class Settings(MenuSzene):
     def __init__(self):
         super().__init__("settings_items", "settings_buttons", "bg_settings.jpg", "Settings")
 
-    def on_mouse_down(self, pos):
+    def on_mouse_down(self, pos, button):
         for button in self.buttons:
              if button.clicked(pos):
                 if button.text == "Back":
@@ -425,7 +491,7 @@ class Credits(MenuSzene):
     def __init__(self):
         super().__init__("credits_items", "credits_buttons", "bg_menu.jpg", "Credits", creditMsg)
     
-    def on_mouse_down(self, pos):
+    def on_mouse_down(self, pos, button):
         for button in self.buttons:
              if button.clicked(pos):
                 if button.text == "Back":
@@ -615,7 +681,7 @@ class GameScene():
     def on_exit(self):
         pass
 
-    def on_mouse_down(self, pos):
+    def on_mouse_down(self, pos, button):
         pass
 
 class GameHomeBase(GameScene):
@@ -809,9 +875,69 @@ def menu_font() -> str:
         return "maturasc"
     else:
         return "stencil"
+    
+def new_game_logik(current_scene):
+    save_name = current_scene.input_text.strip()
+    noise = OpenSimplex(random.randint(0, 1_000_000))
+    if save_name:
+        filename = sanitize_filename(save_name)
+        if filename:
+            save_folder = "saved_games"
+            os.makedirs(save_folder, exist_ok=True)
+            save_path = os.path.join(save_folder, f"{filename}.json")
+            counter = 1
+            if os.path.exists(save_path):  
+                while True:
+                    alt_path = os.path.join(save_folder, f"{filename}_{counter}.json")
+                    if not os.path.exists(alt_path):
+                        save_path = alt_path
+                        break
+                    counter += 1
+
+            save_data = {
+            "name": f"{filename}_{counter}",
+            "created": time.strftime("%Y%m%d%H%M%S", time.gmtime()),
+            "seed": noise._seed,
+            "resources": {
+                "electricity": 70,
+                "metal": 40,
+                "minerals": 85,
+                "water": 20,
+                "communication": 55,
+                "money": 123456,
+                "science": 67890
+            },
+            "resource_max": {
+                "electricity": 100,
+                "metal": 100,
+                "minerals": 100,
+                "water": 100,
+                "communication": 100
+            },
+            "progress": {}
+            }
+
+            with open(save_path, "w", encoding="utf-8") as save_file:
+                json.dump(save_data, save_file, indent=2)
+
+            current_scene.current_save_path = save_path
+            manager.change_scene(GameHomeBase(save_path=save_path))
+            print(f"Created save file: {save_path}")
+
+def continue_game_logik(current_scene):
+    save_name = current_scene.input_text.strip()
+    if save_name:
+        filename = sanitize_filename(save_name)
+        save_path = os.path.join("saved_games", f"{filename}.json")
+        if os.path.exists(save_path):
+            print(f"Continuing game from {save_path}...")
+            manager.change_scene(GameHomeBase(save_path=save_path))
+        else:
+            print(f"No save file found for: {save_name}")
+
 #setzt die buttonkollision für die szenen um
-def on_mouse_down(pos):
-    manager.on_mouse_down(pos)
+def on_mouse_down(pos, button):
+    manager.on_mouse_down(pos, button)
 
 # regelt den input von eingabefeld von aktiver scene
 def sanitize_filename(name: str) -> str:
@@ -840,62 +966,9 @@ def on_key_down(key, mod, unicode):
 
     if key == pygame.K_RETURN:
         if isinstance(scene, NewGame): # erstellt json bei new game
-            save_name = scene.input_text.strip()
-            noise = OpenSimplex(random.randint(0, 1_000_000))
-            if save_name:
-                filename = sanitize_filename(save_name)
-                if filename:
-                    save_folder = "saved_games"
-                    os.makedirs(save_folder, exist_ok=True)
-                    save_path = os.path.join(save_folder, f"{filename}.json")
-                    counter = 1
-                    if os.path.exists(save_path):  
-                        while True:
-                            alt_path = os.path.join(save_folder, f"{filename}_{counter}.json")
-                            if not os.path.exists(alt_path):
-                                save_path = alt_path
-                                break
-                            counter += 1
-
-                    save_data = {
-                    "name": f"{filename}_{counter}",
-                    "created": time.strftime("%Y%m%d%H%M%S", time.gmtime()),
-                    "seed": noise._seed,
-                    "resources": {
-                        "electricity": 70,
-                        "metal": 40,
-                        "minerals": 85,
-                        "water": 20,
-                        "communication": 55,
-                        "money": 123456,
-                        "science": 67890
-                    },
-                    "resource_max": {
-                        "electricity": 100,
-                        "metal": 100,
-                        "minerals": 100,
-                        "water": 100,
-                        "communication": 100
-                    },
-                    "progress": {}
-                    }
-
-                    with open(save_path, "w", encoding="utf-8") as save_file:
-                        json.dump(save_data, save_file, indent=2)
-
-                    scene.current_save_path = save_path
-                    manager.change_scene(GameHomeBase(save_path=save_path))
-                    print(f"Created save file: {save_path}")
+            new_game_logik(scene)
         elif isinstance(scene, ContinueGame): # läd json bei continue game
-            save_name = scene.input_text.strip()
-            if save_name:
-                filename = sanitize_filename(save_name)
-                save_path = os.path.join("saved_games", f"{filename}.json")
-                if os.path.exists(save_path):
-                    print(f"Continuing game from {save_path}...")
-                    manager.change_scene(GameHomeBase(save_path=save_path))
-                else:
-                    print(f"No save file found for: {save_name}")
+            continue_game_logik(scene)
         else:
             print("Entered:", scene.input_text)
         return
